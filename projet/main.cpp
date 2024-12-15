bc11e31b998e31257370502bf97a52e4b44e7efe
Rbaptiste <baptiste.renou@ynov.com>
2024-12-15 14:57:15 +0100

    début affichage menu

---
+++ projet/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Cinema.hpp"
+#include "Movie.hpp"
+#include "Session.hpp"
+#include "Ticket.hpp"
+
+using namespace std;
+
+void AfficherMenu() {
+	cout << "----------------Bienvenue dans votre Cinema !----------------" << endl;
+	cout << "1. Voir la liste des films" << endl;
+	cout << "2. Annuler une reservation" << endl;
+	cout << "3. Quitter" << endl;
+	cout << "Entrez votre choix : ";
+
+
+}
+
+
+int main() {
+
+	//int choix;
+
+	AfficherMenu();
+	//cin >>ouvrirmenu;
+}
