1411c5768e13446eb410f9064166bff22642fd53
cjohann <johann.chiarotto@ynov.com>
2024-12-16 19:43:08 +0100

    ajout modifs main

--- projet/main.cpp
+++ projet/main.cpp
@@ -6,19 +6,43 @@
 
 using namespace std;
 
-void AfficherMenu() {
+int AfficherMenu() {
+	int choix;
 	cout << "----------------Bienvenue dans votre Cinema !----------------" << endl;
 	cout << "1. Voir la liste des films" << endl;
 	cout << "2. Annuler une reservation" << endl;
 	cout << "3. Quitter" << endl;
 	cout << "Entrez votre choix : ";
+	cin >> choix;
+
+	return choix;
 }
 
 
 int main() {
+	bool run = true;
+	while (run == true)
+	{
+		int choix;
+		choix = AfficherMenu();
 
-	//int choix;
-
-	AfficherMenu();
-	//cin >>ouvrirmenu;
+		if (choix == 1)
+		{
+			//programme pour faire une réservation
+		}
+		else if (choix == 2)
+		{
+			//programme pour faire un annule
+		}
+		else if (choix == 3)
+		{
+			run = false;
+		}
+		else
+		{
+			cout << " ";
+			cout << "Il y a une erreur dans votre demmande." << endl;
+			cout << " ";
+		}
+	}
 }
